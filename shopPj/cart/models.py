9344c53eac8c9cfd30e84778ef92a3e94b008b79
RuslanTopal <baranotik15@gmail.com>
2025-03-25 19:25:43 +0000

    some changes to structure of project

--- shopPj/cart/models.py
+++ shopPj/cart/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.conf import settings
+from products.models import Product
 
-# Create your models here.
+# НЕ уверен не должна ли эта переходная таблица создаваться автоматически
+class Cart(models.Model):
+    # id
+    # user_id
+    pass
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name}"
--- shopPj/products/models.py
+++ shopPj/products/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from .users.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField(default=0)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    # img = ??
+
+    def __str__(self):
+        return self.name
+
+
+class Favorite(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ("user", "product")
+
+    def __str__(self):
+        return f"{self.user} - {self.product}"
