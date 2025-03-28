9cf1d07f8e468d6a3af2ab1afffb6d4086857e8a
RuslanTopal <baranotik15@gmail.com>
2025-03-28 14:03:12 +0000

    add product_detail page html + css and viewss + urls

--- shopPj/products/urls.py
+++ shopPj/products/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ProductListView
+from .views import ProductListView, ProductDetailView
 
 urlpatterns = [
     path(
@@ -7,4 +7,9 @@ urlpatterns = [
         ProductListView.as_view(),
         name='product_list'
     ),
+    path(
+        'product/<int:pk>/',
+        ProductDetailView.as_view(),
+        name='product_detail'
+    )
 ]
