eccfd04ad5c5b965f0bea83bcc8e059afd265d83
RuslanTopal <baranotik15@gmail.com>
2025-03-28 19:16:33 +0000

    add login logout + found function and some html and css file

--- shopPj/products/views.py
+++ shopPj/products/views.py
@@ -5,19 +5,33 @@ from products.models import Product, Category
 
 class ProductListView(ListView):
     model = Product
-    template_name = 'product_list.html'
+    template_name = 'products/product_list.html'
     context_object_name = 'products'
 
     def get_queryset(self):
-        category_id = self.kwargs['category_id']
-        category = get_object_or_404(Category, id=category_id)
-        return Product.objects.filter(category=category)
+        queryset = Product.objects.all()
+        category_id = self.kwargs.get('category_id')
+        search_query = self.request.GET.get('search', '')
+
+        if category_id:
+            category = get_object_or_404(Category, id=category_id)
+            queryset = queryset.filter(category=category)
+
+        if search_query:
+            queryset = queryset.filter(name__icontains=search_query)
+
+        return queryset
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        category_id = self.kwargs['category_id']
-        category = get_object_or_404(Category, id=category_id)
-        context['category'] = category
+        category_id = self.kwargs.get('category_id')
+
+        if category_id:
+            category = get_object_or_404(Category, id=category_id)
+            context['category'] = category
+
+        context['search_query'] = self.request.GET.get('search', '')
+
         return context
 
 
@@ -25,4 +39,3 @@ class ProductDetailView(DetailView):
     model = Product
     template_name = 'products/product_detail.html'
     context_object_name = 'products_detail'
-
