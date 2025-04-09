516079213f4028d34cc5da5c3552036c5ee7e937
RuslanTopal <baranotik15@gmail.com>
2025-04-09 17:09:45 +0000

    add some stile for registration form

--- shopPj/users/models.py
+++ shopPj/users/models.py
@@ -3,7 +3,6 @@ from django.db import models
 
 
 class User(AbstractUser):
-    email = models.EmailField(unique=True)
     favorite_products = models.ManyToManyField(
         "products.Product", related_name="favorite_products", blank=True
     )
