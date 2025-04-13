3e0825f2ee0619aabae7d6acf4845ba89c245ef4
RuslanTopal <baranotik15@gmail.com>
2025-04-13 08:36:50 +0000

    change build.sh file

--- shopPj/build.sh
+++ shopPj/build.sh
@@ -3,6 +3,7 @@
 set -o errexit
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
+source .venv/bin/activate
 pip install -r requirements.txt
 
 # Convert static asset files
